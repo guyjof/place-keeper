27e55a342e2f2cfd782437472cfd87ea04fff034
guyjof <guyjofre02@gmail.com>
2021-06-05 22:44:49 +0300

    css update

--- js/appController.js
+++ js/appController.js
@@ -17,7 +17,7 @@ function initMap(lat, lng) {
         gestureHandling: "cooperative"
     });
 
-    var marker = new google.maps.Marker({
+    var playerMarker = new google.maps.Marker({
         position: { lat: 29.55823779167494, lng: 34.9526631121632 },
         map,
         title: 'You Are Here',
@@ -26,6 +26,24 @@ function initMap(lat, lng) {
             scaledSize: new google.maps.Size(40, 40)
         },
         animation: google.maps.Animation.DROP
+    });
+
+    var pokeballMarker = new google.maps.Marker({
+        position: { lat, lng },
+        map,
+        title: 'Saved',
+        icon: {
+            url: '/img/pokeball.png',
+            scaledSize: new google.maps.Size(40, 40)
+        },
+        animation: google.maps.Animation.DROP
+    });
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
     });
 }
 
